d5523875839f152abd5fc9f4aba20ab2c7754007
SixArm <sixarm@sixarm.com>
2011-11-29 22:51:57 -0800

    Publish

---
+++ lib/sixarm_ruby_date_stamp.rb
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+=begin rdoc
+Please see README.rdoc
+=end
+
+
+class Date
+
+
+  # @return [String] a date stamp string in standard format: "YYYY-MM-DD"
+  #
+  # This standard format is specified in IETF RFC 3339 and ISO 8601.
+  #
+  # @see http://www.ietf.org/rfc/rfc3339.txt
+  #
+  # @example
+  #   Date.now.stamp 
+  #   => "2010-12-31" 
+
+  def stamp
+    strfdate('%Y-%m-%d %H:%M:%SZ')
+  end
+
+
+  # Shorthand for Date.today.stamp
+  #
+  # @example
+  #   Date.stamp
+  #    => "2010-12-31" 
+  #
+  # @return [String] Date.today.stamp
+
+  def self.stamp
+    today.stamp
+  end
+
+
+end
---
+++ test/sixarm_ruby_date_stamp_test.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+require 'test/unit'
+require 'simplecov'
+SimpleCov.start
+require 'sixarm_ruby_date_stamp'
+
+class DateTest < Test::Unit::TestCase
+
+ def test_stamp_with_class_method
+   d=Date.stamp
+   assert(d=~/^\d\d\d\d-\d\d-\d\d$/,d)
+ end
+
+ def test_stamp_with_instance_method
+   d=Date.today.stamp
+   assert(d=~/^\d\d\d\d-\d\d-\d\d$/,d)
+ end
+ 
+end
+
